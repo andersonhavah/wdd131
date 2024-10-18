d0269218dd5170b1bde8cf443fa97e13bca09f6d
andersonhavah <havahanderson@gmail.com>
2024-10-18 20:03:55 +0000

    Temple Album

--- scripts/temples.js
+++ scripts/temples.js
@@ -10,3 +10,12 @@ currentYear.innerHTML = today.getFullYear();
 
 // Output the date and time of the last modification
 lastModification.innerHTML = `Last Modification: ${document.lastModified} `;
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
